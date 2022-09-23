fb22f36255e68d48e4225787c80e54124721d028
Emmanuel Byrd <25442086+EByrdS@users.noreply.github.com>
2022-09-23 14:30:47 +0100

    Reduces example

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -2,10 +2,7 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ModuleA } from './moduleA/module.module';
-// import { ModuleB } from './moduleB/module.module';
 
 @Module({
   imports: [
@@ -14,9 +11,8 @@ import { ModuleA } from './moduleA/module.module';
       driver: ApolloDriver,
     }),
     ModuleA,
-    // ModuleB,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
